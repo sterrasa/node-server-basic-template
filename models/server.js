3227da71d4297b0538ed4b44a5d903dcd59f07f6
sterrasa <80227106+sterrasa@users.noreply.github.com>
2022-05-12 12:12:01 +0200

    Node server crud user
    * add mongo connection
    
    * remove node
    
    * remove node
    
    * crud user
    
    * remove node_modules
    
    * remove sensitive info
    
    * remove sensitive info
    
    * remove sensitive info
    
    * crud
    
    * Delete .env

--- models/server.js
+++ models/server.js
@@ -1,43 +1,48 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConection } = require('../database/config.db');
 
 class Server {
 
     constructor() {
-        this.app  = express();
+        this.app = express();
         this.port = process.env.PORT;
         this.usersPath = '/api/users';
-
+        //connect db
+        this.connectDb();
         // Middlewares
         this.middlewares();
-
         // Rutas de mi aplicación
         this.routes();
+    }
+
+
+    async connectDb() {
+        await dbConection();
     }
 
     middlewares() {
 
         // CORS
-        this.app.use( cors() );
+        this.app.use(cors());
 
         // read and parse json
-        this.app.use( express.json() );
+        this.app.use(express.json());
 
         // public dir
-        this.app.use( express.static('public') );
+        this.app.use(express.static('public'));
 
     }
 
     routes() {
-        this.app.use( this.usersPath, require('../routes/users.routes') );
+        this.app.use(this.usersPath, require('../routes/users.routes'));
     }
 
     listen() {
-        this.app.listen( this.port, () => {
-            console.log('server running on port: ', this.port );
+        this.app.listen(this.port, () => {
+            console.log('server running on port: ', this.port);
         });
     }
 
 }
-
 module.exports = Server;
