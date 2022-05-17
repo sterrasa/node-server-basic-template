3c6a6fb0adcbcaf15f2b0f38655a2b6bce39c810
Sebastian <sebastian.terrasa@gmail.com>
2022-05-17 15:37:21 +0200

    google sign in

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { doLogin } = require('../controllers/auth.controller');
+const { doLogin, googleSignIn } = require('../controllers/auth.controller');
 const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
@@ -12,5 +12,12 @@ router.post('/login', [
     check('password', "password is mandatory").not().isEmpty(),
     validateFields
 ], doLogin)
+
+router.post('/google', [
+    check('id_token', "token is mandatory").not().isEmpty(),
+    validateFields
+], googleSignIn)
+
+
 
 module.exports = router;
